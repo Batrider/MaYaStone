83a3bb286c46f703ad88688cfe1f59797c95ac97
陈濠 <chenh@feiyu.com>
2017-07-07 22:04:23 +0800

    关卡可编辑

---
+++ MaYaStone/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("加速度因子")]
+    private float accelerFactor;
+    Rigidbody player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        player.AddForce(accelerFactor * new Vector3(Input.acceleration.x, 0, Input.acceleration.y));
+#if UNITY_EDITOR
+        if(Input.GetMouseButton(0))
+        {
+            player.AddForce(Vector3.left);
+        }
+        else if(Input.GetMouseButton(1))
+        {
+            player.AddForce(Vector3.right);
+        }
+#endif
+    }
+}
