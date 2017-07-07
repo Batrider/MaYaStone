3c3100e63181b923cae033e3ec0c08f0aa9c2758
陈濠 <chenh@feiyu.com>
2017-07-07 22:15:01 +0800

    ==关卡可编辑==

---
+++ MaYaStone/Assets/Script/Manager/CameraFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public float distance;
+    public Transform target;
+    void Start()
+    {
+        transform.position = target.transform.position + distance * Vector3.up;
+    }
+    public void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+        }
+    }
+}
---
+++ MaYaStone/Assets/Script/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    public enum PlayerState
+    {
+        Normal,//
+        God,//无敌
+        Dead,//死亡
+    }
+    [SerializeField]
+    private float deadLine = -12;
+    PlayerState state = PlayerState.Normal;
+    PlayerController controller;
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    public void LateUpdate()
+    {
+        if (transform.position.y < deadLine)
+        {
+            Debug.Log("dead!");
+
+            SceneManager.LoadScene("main");
+        }
+    }
+}
