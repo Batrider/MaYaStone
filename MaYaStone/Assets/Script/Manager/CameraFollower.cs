ac4e10b66b5aeab2e0b2be34c45d67c618cbd60e
陈濠 <chenh@feiyu.com>
2017-07-08 16:24:39 +0800

    弹簧

--- MaYaStone/Assets/Script/Manager/CameraFollower.cs
+++ MaYaStone/Assets/Script/Manager/CameraFollower.cs
@@ -9,12 +9,13 @@ public class CameraFollower : MonoBehaviour
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.position = new Vector3(target.position.x, target.transform.position.y + distance.y, target.transform.position.z + distance.z);
     }
     public void LateUpdate()
     {
         if (target != null)
         {
-            transform.position = new Vector3(target.position.x, target.transform.position.y + distance.y, target.transform.position.z + distance.z);
+            transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, transform.position.y, target.transform.position.z + distance.z), Time.deltaTime);
         }
     }
 }
