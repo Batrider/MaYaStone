a2223af5e35420cf7cf9b2564c53fcb72e181e75
陈濠 <chenh@feiyu.com>
2017-07-09 03:19:44 +0800

    移动砖块.prefab

---
+++ MaYaStone/Assets/Script/Obstacle/Movetrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movetrap : MonoBehaviour {
+    
+    public Vector3 endPosition;
+    public int time;
+    public iTween.EaseType easyType;
+    // Use this for initialization
+    void Start () {
+        Hashtable args = new Hashtable();
+        args.Add("easeType", easyType);
+        args.Add("time", time);
+        args.Add("loopType", "pingPong");
+        args.Add("position", endPosition);
+        args.Add("islocal", true);
+
+        iTween.MoveFrom(gameObject, args);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
