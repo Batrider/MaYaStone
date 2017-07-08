dee70b4382a3701146f77bc5e5a6161b5ba9b106
陈濠 <chenh@feiyu.com>
2017-07-08 16:42:53 +0800

    加速带.prefab

--- MaYaStone/Assets/Script/Obstacle/AcceleratingRibbon.cs
+++ MaYaStone/Assets/Script/Obstacle/AcceleratingRibbon.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class AcceleratingRibbon : MonoBehaviour
 {
     public Vector3 force;
+    public Material material;
+    public float Speed = 1;
+    public void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+
+    }
+
     // Use this for initialization
+
     public void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             Rigidbody body = other.GetComponent<Rigidbody>();
             body.AddForce(force, ForceMode.Acceleration);
         }
+    }
+
+    public void Update()
+    {
+        material.mainTextureOffset -= Vector2.up * Speed * Time.deltaTime;
     }
 }
