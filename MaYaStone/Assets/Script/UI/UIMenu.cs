6f8e5e0c97a2eacd41aab5778a2ab205674c49d0
陈濠 <chenh@feiyu.com>
2017-07-09 04:56:43 +0800

    关卡选择界面

---
+++ MaYaStone/Assets/Script/UI/UIMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMenu : MonoBehaviour {
+
+    public UILabel progressLb;
+    public UITexture uiTexture;
+    public UIScrollView scrollView;
+    public UICenterOnChild centerChild;
+    public List<UIStageItem> stageList = new List<UIStageItem>();
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < stageList.Count; i++)
+        {
+            int index = i;
+            UIEventListener.Get(stageList[index].boxColliderObj).onClick = (x) => OpenStage(stageList[index]);
+        }
+        centerChild.onFinished += OnCenterFinish;
+    }
+
+    void OpenStage(UIStageItem stage)
+    {
+        if (stage.stageIndex == 1)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+    void OnCenterFinish()
+    {
+        UIStageItem item = centerChild.centeredObject.GetComponent<UIStageItem>();
+        uiTexture.mainTexture = item.bgTexture;
+        progressLb.text = string.Format("{0}%", item.stageIndex);
+    }
+}
---
+++ MaYaStone/Assets/Script/UI/UIStageItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStageItem : MonoBehaviour {
+
+    public GameObject boxColliderObj;
+    public int stageIndex;
+    public Texture bgTexture;
+}
